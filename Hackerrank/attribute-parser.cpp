dc2fb9c86257a7a139beec3e69e2026cc4397721
mach1r0 <segundacontafd@gmail.com>
2024-06-07 11:54:54 -0300

    adding exercise of day on leetcode

---
+++ Hackerrank/attribute-parser.cpp
@@ -0,0 +1,29 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int N, Q; cin >> N >> Q;    
+    vector<string> str; 
+    vector<string> query; 
+    for (int i = 0; i < N; i++){
+        string temp;
+        cin >> temp;
+        str.push_back(temp); 
+    }   
+    for (int i = 0; i < Q; i++){
+        string temp; 
+        cin >> temp; 
+        query.push_back(temp);
+    }
+    for (int i = 0; i < Q; i++){
+        
+    }
+       
+    
+    
+    return 0;
+}
